5829d7450f18fab587ebb8798d6f78282d2ec30d
Ayanami Chicken <jiadong.20@intl.zju.edu.cn>
2023-11-18 02:11:37 +0800

    Update receiver_main.cpp
    implement the receiver logic and defined the packet format of TCP transfer.

--- MP2/mp2/src/receiver_main.cpp
+++ MP2/mp2/src/receiver_main.cpp
@@ -14,10 +14,14 @@
 #include <cstring>
 
 #define DATA_SIZE 2000
-#define BUFF_SIZE 600000
-#define TOT_COUNT 300
+#define BUFF_SIZE 1000
 
 using namespace std;
+
+void diep(char *s) {
+    perror(s);
+    exit(1);
+}
 
 struct sockaddr_in si_me, si_other;
 int s, slen;
@@ -29,6 +33,13 @@ socklen_t addrlen;
 // FIN: 0; DATA: 1; FINACK:2; ACK:3.
 enum PacketType { FIN, DATA, FINACK, ACK };
 
+/* This struct is for the priority queue */
+struct compare {
+    bool operator()(packet a, packet b) {
+        return  a.seq_num > b.seq_num; 
+    }
+};
+
 typedef struct{
     int 	data_size;
     int 	seq_num;
@@ -36,6 +47,8 @@ typedef struct{
     PacketType msg_type; //DATA 0 SYN 1 ACK 2 FIN 3 FINACK 4
     char    data[DATA_SIZE];
 }packet;
+
+priority_queue<packet, vector<packet>, compare> pqueue;
 
 void send_ack(int ack_idx, PacketType ack_type) {
     // Implement the logic to send ACK
@@ -46,11 +59,6 @@ void send_ack(int ack_idx, PacketType ack_type) {
     if (sendto(s, &ack, sizeof(packet), 0, (sockaddr*)&si_other, (socklen_t)sizeof (si_other))==-1){
         diep("ACK's sendto()");
     }
-}
-
-void diep(char *s) {
-    perror(s);
-    exit(1);
 }
 
 void reliablyReceive(unsigned short int myUDPport, char* destinationFile) {
@@ -76,6 +84,47 @@ void reliablyReceive(unsigned short int myUDPport, char* destinationFile) {
         diep("Cannot open the destination file");
     }
 
+    while (true){
+        packet recv_pkt;
+        if (recvfrom(s, &recv_pkt, sizeof(packet), 0, (sockaddr*)&si_other, (socklen_t*)&slen) == -1)
+            diep("recvfrom()");
+        // Finish receiving when FIN. End Transmission.
+        if (recv_pkt.msg_type == FIN){
+            cout << "receiving FIN" <<endl;
+            send_ack(ack_index, FINACK);
+            break;
+        }
+        else if (recv_pkt.msg_type == DATA){
+            // receive duplicated pkt.
+            if (recv_pkt.seq_num < ack_index){
+                cout << "receive duplicated pkt." << endl;
+            }
+            // receive out of order pkt
+            else if (recv_pkt.seq_num > ack_index){
+                if (pqueue.size() < BUFF_SIZE){
+                    pqueue.push(recv_pkt);
+                    ack_index++;
+                } else {
+                    cout << "Buffer Queue is full" << endl;
+                }
+            }
+            // receive in-order pkt.
+            else{
+                // Write data to file.
+                fwrite(recv_pkt.data, sizeof(char), recv_pkt.data_size, fp);
+                ack_index += recv_pkt.data_size;
+                // only in order and in queue will be written to file.
+                while (!pqueue.empty() && pqueue.top().seq_num == ack_index){
+                    packet pkt = pqueue.top();
+                    fwrite(pkt.data, sizeof(char), pkt.data_size, fp);
+                    ack_index += pkt.data_size;
+                    pqueue.pop();
+                }
+            }
+            send_ack(ack_index, ACK);
+        }
+    }
+    fclose(fp);
     close(s);
 	printf("%s received.", destinationFile);
     return;
