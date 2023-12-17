96a5b2f2a5642e90602e308b1a00962ca3ce0ba3
Jade (Jiadong) Hong <jiadong.20@intl.zju.edu.cn>
2023-12-18 01:58:39 +0800

    Add and tested util function and pretty print function for link state, link state nearly finished.

---
+++ MP3/mp3/src/linkstate.cpp
@@ -0,0 +1,364 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <queue>
+#include <limits>
+#include <stack>
+#include <functional>
+
+using namespace std;
+
+const int INF = numeric_limits<int>::max();
+
+// Node Structure.
+struct Node {
+    int id;
+    vector<pair<int, int>> neighbors;  // 保存邻接节点的 {权重, 目标节点ID} 对
+
+    Node(int _id) : id(_id) {}
+
+    // Add Edge.
+    void addEdge(int weight, int neighborId) {
+        neighbors.push_back({weight, neighborId});
+        return;
+    }
+
+    // Delete Specific Edge.
+    void deleteEdge(int neighborId) {
+        auto it = find_if(neighbors.begin(), neighbors.end(),
+                          [neighborId](const pair<int, int>& edge) {
+                              return edge.second == neighborId;
+                          });
+
+        if (it != neighbors.end()) {
+            neighbors.erase(it);
+        }
+        return;
+    }
+};
+
+// util data structure for loading from file.
+struct linkedListNode{
+    int u;
+    int v;
+    int weight;
+    linkedListNode* prev;
+    linkedListNode* next;
+};
+
+// util data structure for loading messages.
+struct MessagelinkedListNode{
+    int src;
+    int dest;
+    string message;
+    MessagelinkedListNode* prev;
+    MessagelinkedListNode* next;
+};
+
+// Class Graph
+class Graph {
+public:
+    vector<Node> nodes;
+    vector<int> distances;
+    vector<int> predecessors;
+    linkedListNode* topoChangeInfo;
+    MessagelinkedListNode* msgInfo;
+
+    // add & Delete undirected edge in graph
+    void addUndirectedEdge(int u, int v, int weight) {
+        nodes[u].addEdge(weight, v);
+        nodes[v].addEdge(weight, u);
+        return;
+    }
+
+    void deleteUndirectedEdge(int u, int v){
+        nodes[u].deleteEdge(v);
+        nodes[v].deleteEdge(u);
+        return;
+    }
+
+    // Dijkstra Algorithm
+    void dijkstra(int start) {
+        int n = nodes.size();
+        distances.assign(n, INF);
+        predecessors.assign(n, -1);
+        distances[start] = 0;
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+        pq.push({0, start});
+
+        while (!pq.empty()) {
+            int u = pq.top().second;
+            int dist_u = pq.top().first;
+            pq.pop();
+
+            if (dist_u > distances[u]) {
+                continue;
+            }
+
+            for (const pair<int, int>& neighbor : nodes[u].neighbors) {
+                int v = neighbor.second;
+                int weight_uv = neighbor.first;
+
+                if (distances[u] + weight_uv < distances[v]) {
+                    distances[v] = distances[u] + weight_uv;
+                    predecessors[v] = u;
+                    pq.push({distances[v], v});
+                }
+            }
+        }
+        return;
+    }
+
+    // Print out the shortest path
+    void printShortestPaths(int start) {
+        int n = distances.size();
+
+        for (int i = 1; i < n; ++i) {
+            cout << "Shortest distance from node " << start << " to node " << i << " is: ";
+            if (distances[i] == INF) {
+                cout << "INFINITY" << endl;
+            } else {
+                cout << distances[i] << " and the path is: ";
+                stack<int> path;
+                int current = i;
+                while (current != -1) {
+                    path.push(current);
+                    current = predecessors[current];
+                }
+                while (!path.empty()) {
+                    cout << path.top() << " ";
+                    path.pop();
+                }
+                cout << endl;
+            }
+        }
+        return;
+    }
+
+    // Load Graph edge and vertices from file.
+    void loadGraphFromFile(const string& filename) {
+        ifstream file(filename);
+        if (!file.is_open()) {
+            cerr << "Error: Could not open file " << filename << endl;
+            return;
+        }
+
+        int u, v, weight;
+        linkedListNode* lastNode;
+        linkedListNode* prevNode = nullptr;
+        int maxIdx = 0;
+        // construct the linked list to cache the data.
+        while (file >> u >> v >> weight) {
+            if (u > maxIdx){
+                maxIdx = u;
+            }
+            if (v > maxIdx){
+                maxIdx = v;
+            }
+            lastNode = new linkedListNode;
+            // handle initial condition.
+            if (prevNode != nullptr) {
+                prevNode->next = lastNode;
+                lastNode->prev = prevNode;
+            } else {
+                lastNode->prev = nullptr;
+            }
+            lastNode->u = u;
+            lastNode->v = v;
+            lastNode->weight = weight;
+            lastNode->next = nullptr;
+            prevNode = lastNode;
+        }
+
+        file.close();
+
+        for (int i = 0; i <= maxIdx; i++){
+            nodes.push_back(Node(i));
+        }
+
+        linkedListNode* currentNode = lastNode;
+        while (currentNode != nullptr){
+            addUndirectedEdge(currentNode->u, currentNode->v, currentNode->weight);
+            currentNode = currentNode->prev;
+            if (currentNode != nullptr){
+                delete currentNode->next;
+            }
+        }
+
+        return;
+    }
+
+    void loadChangeFromFile(const string& filename) {
+        ifstream file(filename);
+        if (!file.is_open()) {
+            cerr << "Error: Could not open file " << filename << endl;
+            return;
+        }
+
+        int u, v, weight;
+        linkedListNode* lastNode;
+        linkedListNode* prevNode = nullptr;
+        // construct the linked list to cache the data.
+        while (file >> u >> v >> weight) {
+            lastNode = new linkedListNode;
+            // handle initial condition.
+            if (prevNode != nullptr) {
+                prevNode->next = lastNode;
+                lastNode->prev = prevNode;
+            } else {
+                lastNode->prev = nullptr;
+                topoChangeInfo = lastNode;
+            }
+            lastNode->u = u;
+            lastNode->v = v;
+            lastNode->weight = weight;
+            lastNode->next = nullptr;
+            prevNode = lastNode;
+        }
+
+        file.close();
+        return;
+    }
+
+    void loadMessageFromFile(const string& filename){
+        ifstream file(filename);
+        if (!file.is_open()) {
+            cerr << "Error: Could not open file " << filename << endl;
+            return;
+        }
+
+        int source, destination;
+        MessagelinkedListNode* lastNode;
+        MessagelinkedListNode* prevNode = nullptr;
+        string message;
+        while (file >> source >> destination) {
+            getline(file, message);
+            // remove the blanks ahead of string.
+            message = message.substr(1);
+            lastNode = new MessagelinkedListNode;
+            // handle initial condition
+            if (prevNode != nullptr) {
+                prevNode->next = lastNode;
+                lastNode->prev = prevNode;
+            } else {
+                lastNode->prev = nullptr;
+                msgInfo = lastNode;
+            }
+            lastNode->src = source;
+            lastNode->dest = destination;
+            lastNode->message = message;
+            lastNode->next = nullptr;
+            prevNode = lastNode;
+        }
+
+        file.close();
+        return;
+    }
+
+    void initGraph(const string& topofile, const string& changesfile, const string& messagefile){
+        loadGraphFromFile(topofile);
+        loadChangeFromFile(changesfile);
+        loadMessageFromFile(messagefile);
+        return;
+    }
+
+    void printForwardingTable(int src) {
+        int n = distances.size();
+        for (int i = 1; i < n; i ++) {
+            if (i == src){
+                cout << i << " " << i << " " << "0" << endl;
+            } else {
+                if (distances[i] == INF) {
+                    continue;
+                } else {
+                    cout << i << " ";
+                    int current = i;
+                    while (predecessors[current] != src) {
+                        current = predecessors[current];
+                    }
+                    cout << current << " " << distances[i] << endl;
+                }
+            }
+        }
+        return;
+    }
+
+    void printMsgTrans(){
+        MessagelinkedListNode* currentMsgInfo = msgInfo;
+        while (currentMsgInfo != nullptr){
+            
+            // int n = distances.size();
+
+            // for (int i = 1; i < n; ++i) {
+            //     cout << "Shortest distance from node " << start << " to node " << i << " is: ";
+            //     if (distances[i] == INF) {
+            //         cout << "INFINITY" << endl;
+            //     } else {
+            //         cout << distances[i] << " and the path is: ";
+            //         stack<int> path;
+            //         int current = i;
+            //         while (current != -1) {
+            //             path.push(current);
+            //             current = predecessors[current];
+            //         }
+            //         while (!path.empty()) {
+            //             cout << path.top() << " ";
+            //             path.pop();
+            //         }
+            //         cout << endl;
+            //     }
+            // }
+            
+            // cout << "from " << currentMsgInfo->src << "to " << currentMsgInfo->dest << "hops ";
+
+        }
+    }
+};
+
+int main() {
+    // Establish Graph and add edges.
+    Graph graph;
+    // for (int i = 0; i < 6; ++i) {
+    //     graph.nodes.push_back(Node(i));
+    // }
+
+    // graph.addUndirectedEdge(0, 1, 2);
+    // graph.addUndirectedEdge(0, 4, 5);
+    // graph.addUndirectedEdge(1, 2, 1);
+    // graph.addUndirectedEdge(1, 3, 7);
+    // graph.addUndirectedEdge(2, 4, 2);
+    // graph.addUndirectedEdge(3, 5, 3);
+    // graph.addUndirectedEdge(4, 5, 1);
+    graph.initGraph("./topofile","./changesfile","./Messagefile");
+
+    int start_node = 2;
+
+    graph.dijkstra(start_node);
+    graph.printForwardingTable(2);
+
+    // print out the shortest path.
+    // graph.printShortestPaths(start_node);
+
+    return 0;
+}
+
+
+// int main(int argc, char** argv) {
+//     //printf("Number of arguments: %d", argc);
+//     if (argc != 4) {
+//         printf("Usage: ./linkstate topofile messagefile changesfile\n");
+//         return -1;
+//     }
+
+//     FILE *fpOut;
+//     fpOut = fopen("output.txt", "w");
+//     fclose(fpOut);
+
+
+//     return 0;
+// }
